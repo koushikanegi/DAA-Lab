5ed94f562c0cc8b2b2221214879dafe0769f7dda
Koushika Negi <54894442+koushikanegi@users.noreply.github.com>
2021-11-14 00:31:38 +0530

    Add files via upload

---
+++ week-2/week2q2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void function1(int arr[],int n)
+{
+    int flag=-1;
+    int sum=0;
+    for(int i=0;i<n-2;++i)
+    {
+        for(int j=i+1;j<n-1;++j)
+        {
+            sum=arr[i]+arr[j];
+            for(int k=j+1;k<n;++k)
+            {
+                if(sum==arr[k])
+                {
+                    flag=1;
+                    cout<<++i<<" + "<<++j<<" = "<<++k<<endl;
+                }
+            }
+        }
+    }
+    if(flag==-1)
+        cout<<"No sequence found";
+}
+
+int main()
+{
+    cout<<"Enter test cases : " ;
+    int t;
+    cin>>t;
+    while(t--){
+        cout<<"Enter array size " ;
+        int n;
+        cin>>n;
+        int arr[n];
+        cout<<"Enter elements : ";
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        function1(arr,n);
+        cout<<endl;
+    }
+}
